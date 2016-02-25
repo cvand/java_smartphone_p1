77c39138cdbc08a6587915ddf3343af8dc141007
cvand <vandera.chryssanthi@gmail.com>
2016-02-25 03:50:33 -0800

    refactored client

--- src/client/DefaultSocketClient.java
+++ src/client/DefaultSocketClient.java
@@ -61,6 +61,10 @@ public class DefaultSocketClient extends Thread implements SocketClientInterface
 	public void handleSession() {
 		Object obj = null;
 		while ((obj = deserializeObject()) != null) {
+			if ((obj instanceof String) && ((String) obj).equals("Bye!")) {
+				break;
+			}
+			
 			handleInput(obj);
 		}
 	}
